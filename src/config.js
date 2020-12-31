2106d959f9bee829b71cb77d229566983f82a4df
Lara Samuelson <lara.samuelson@gmail.com>
2020-12-30 16:27:38 -0800

    typo

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
 export default {
-
     API_ENDPOINT:'https://git.heroku.com/nameless-ravine-02650.git/api',
     API_KEY_PHOTO: process.env.REACT_APP_PHOTO_API_KEY,
     API_KEY: process.env.REACT_APP_API_KEY,
